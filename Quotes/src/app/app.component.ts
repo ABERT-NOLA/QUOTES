9f87c375f103ef27e430715348f134ffd9bd94f9
Albert Obwoge <albertnyambegaa@gmail.com>
2020-08-23 13:47:52 +0300

    added component quote

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote} from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes: string [];
-  constructor () {
-    this.quotes = ['You can do anything, but not everything' ,'The richest man is not he who has the most, but he who needs the least' ,'You miss 100 percent of the shots you never take']
-  }
+  quotes: Quote [] = [
+    { id:1, name: 'You can do anything, but not everything'},
+    { id:2, name: 'The richest man is not he who has the most, but he who needs the least'},
+    { id:3, name: 'You miss 100 percent of the shots you never take'},
+  ];
 }
