5471d3f80dea9ca2b0976021645ac3fd01dc47bc
Albert Obwoge <albertnyambegaa@gmail.com>
2020-08-23 16:42:34 +0300

    updates to the files

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -8,8 +8,11 @@ import { Quote} from './quote';
 })
 export class AppComponent {
   quotes: Quote [] = [
-    { id:1, name: 'You can do anything, but not everything',description:'Find an online version and watch merlin find his son'},
-    { id:2, name: 'The richest man is not he who has the most, but he who needs the least',description:'Find an online version and watch merlin find his son'},
-    { id:3, name: 'You miss 100 percent of the shots you never take',description:'Find an online version and watch merlin find his son'},
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
+    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
+    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon'),
+    new Quote(4,'Get Dog Food','Pupper likes expensive snacks'),
+    new Quote(5,'Solve math homework','Damn Math'),
+    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord'),
   ];
 }
--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -9,12 +9,17 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    { id:1, name: 'You can do anything, but not everything',description:'Find an online version and watch merlin find his son'},
-    { id:2, name: 'The richest man is not he who has the most, but he who needs the least',description:'Find an online version and watch merlin find his son'},
-    { id:3, name: 'You miss 100 percent of the shots you never take',description:'Find an online version and watch merlin find his son'},
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
+    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
+    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon'),
+    new Quote(4,'Get Dog Food','Pupper likes expensive snacks'),
+    new Quote(5,'Solve math homework','Damn Math'),
+    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord'),
 
   ];
-
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
   constructor() { }
 
   ngOnInit(): void {
