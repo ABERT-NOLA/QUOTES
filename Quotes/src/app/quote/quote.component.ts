56db6b0d5adfb9868789fb15e5e97570a8c574a1
Albert Obwoge <albertnyambegaa@gmail.com>
2020-08-23 14:13:04 +0300

    added component quote

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[]=[
+    { id:1, name: 'You can do anything, but not everything'},
+    { id:2, name: 'The richest man is not he who has the most, but he who needs the least'},
+    { id:3, name: 'You miss 100 percent of the shots you never take'},
+
+  ];
 
   constructor() { }
 
