fef29e59f9c4e87c857621c1ba27bb5d8cd4a122
Albert Obwoge <albertnyambegaa@gmail.com>
2020-08-23 21:37:56 +0300

    updates to quotes

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2020,3,14)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2020,3,14)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2020,3,14)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2020,3,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2020,3,14)),
+    new Quote(1, 'You can do anything, but not everything.', 'David Allen',new Date(2020,3,14)),
+    new Quote(2,'You miss 100 percent of the shots you never take.','Wayne Gretzky',new Date(2020,3,14)),
+    new Quote(3,'You must be the change you wish to see in the world','Gandhi',new Date(2020,3,14)),
+    new Quote(4,'Do not seek to follow in the footsteps of the men of old; seek what they sought','Basho',new Date(2020,3,14)),
+    new Quote(5,'A wise man gets more use from his enemies than a fool from his friends.','Baltasar Gracian',new Date(2020,3,14)),
+    new Quote(6,'We are what we repeatedly do; excellence, then, is not an act but a habit','Aristotle',new Date(2020,3,14)),
 
   ];
   deleteQuote(isComplete, index){
